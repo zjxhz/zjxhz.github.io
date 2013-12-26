cfb88e7d5c2ea9c80e2b68a05188c487a67ed0fd
zjxhz <xuhuanze@qq.com>
2013-12-26 15:16:59 +0800

    new post and srcipts for running server and creating new post

---
+++ newpost.py
@@ -0,0 +1,21 @@
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('title', help='title of the post')
+args=parser.parse_args()
+
+from datetime import datetime
+dt = datetime.now().strftime('%Y-%m-%d')
+file_name="_posts/%s_%s.md" % (dt, args.title)
+f = open(file_name, 'w')
+f.write('---\n')
+f.write('layout: post\n')
+f.write('title: \n')
+f.write('description: \n')
+f.write('category: articles\n')
+f.write('tags: \n')
+f.write('---\n')
+f.close()
+
+from subprocess import call
+call(['vim', file_name])
+
